d43518179ec3e745c7e459cad7434cda53d72f16
aimeeso <aimee18222724@yahoo.com.hk>
2023-03-30 21:45:25 +0800

    Added Content seeder and added seeders in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            PackSeeder::class,
+            ContentSeeder::class,
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
