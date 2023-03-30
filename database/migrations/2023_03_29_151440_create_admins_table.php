0238bbe36e0bce54d08ae1e2d043f191d580270d
aimeeso <aimee18222724@yahoo.com.hk>
2023-03-30 21:42:37 +0800

    Added admin table and added 2fa fields in users table

--- database/migrations/2023_03_29_151440_create_admins_table.php
+++ database/migrations/2023_03_29_151440_create_admins_table.php
@@ -11,15 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->tinyInteger('tfa_enabled')->default(1);
-            $table->string('tfa_code', 12)->nullable();
-            $table->timestamp('tfa_expired_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
@@ -30,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
     }
 };
